4761e2ba415a5b8b5638135bf4970976ceaa3a78
Adam Stefek <D21735@oaopava.cz>
2024-01-25 09:37:05 +0100

    25.1

--- staticka/ukol1/narozeniny/views.py
+++ staticka/ukol1/narozeniny/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "narozeniny/index.html")
+
+def form(request):
+    return render(request, "narozeniny/form.html")
+
+ 
+
--- staticka/ukol1/ukol1/urls.py
+++ staticka/ukol1/ukol1/urls.py
@@ -19,5 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('narozeniny/', include("narozeniny.urls"))
+    path('narozeniny/', include("narozeniny.urls")),
+    path('narozeniny/form.html', include("narozeniny.urls"))
+
 ]
